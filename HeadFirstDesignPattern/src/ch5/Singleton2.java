89dd20cbc7cb3de1b73816aa5d64218608b07423
Weiwei <ww.sun@outlook.com>
2014-09-23 15:08:11 +0800

    design pattern: singleton pattern
    its problem and three possible solutio
    see singleton*.java

---
+++ HeadFirstDesignPattern/src/ch5/Singleton2.java
@@ -0,0 +1,14 @@
+package ch5;
+
+public class Singleton2 {
+	
+	private static Singleton2 uniqueInstance = new Singleton2();
+	
+	private Singleton2() {
+		
+	}
+	
+	public static Singleton2 getInstance() {
+		return uniqueInstance;
+	}
+}
